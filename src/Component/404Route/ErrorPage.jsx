fcef421ec6925f4457ef010af9c8c846e388a5a8
Sizan458 <www.shijan23@gmail.com>
2023-11-04 22:52:53 +0600

    style the 404 route

--- src/Component/404Route/ErrorPage.jsx
+++ src/Component/404Route/ErrorPage.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h3>hhhhhh</h3>
+        <div className="flex justify-center  align-middle mt-[10%]">
+           <div className="card card-compact   bg-transparent  dark:bg-emerald-500">
+  <figure><img src="https://i.ibb.co/pR9wKyD/404.gif" className="h-[350px]" /></figure>
+  <div className="card-body">
+    
+    <div className="card-actions justify-center ">
+      <Link to='/'>
+      <button className="btn btn-error w-full text-xl  text-black">Go to home</button>
+      </Link>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
