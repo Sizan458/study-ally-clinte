15be923b20a682fcadd665a7b3ec7be91bb93fc8
Sizan458 <www.shijan23@gmail.com>
2023-11-05 19:18:20 +0600

    add footer in login page

--- src/Component/LoginPage/Login/Login.jsx
+++ src/Component/LoginPage/Login/Login.jsx
@@ -1,9 +1,13 @@
+import Footer from "../../Footer/Footer";
 import LogForm from "../LogForm/LogForm";
 
 const Login = () => {
     return (
         <div>
            <LogForm></LogForm>
+           <div className="w-[97%] mx-auto mt-2">
+            <Footer></Footer>
+           </div>
         </div>
     );
 };
