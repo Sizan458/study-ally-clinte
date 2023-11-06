14ca4c90872e7a59f79d855a3197819765df7bcf
Sizan458 <www.shijan23@gmail.com>
2023-11-06 12:37:33 +0600

    add register function of firebase to register page

--- src/Component/RegisterPage/From/From.jsx
+++ src/Component/RegisterPage/From/From.jsx
@@ -1,8 +1,51 @@
-import Lottie from "lottie-react";
+
+import { useContext, useState } from "react";import Lottie from "lottie-react";
 import registerAnimation from "../../../assets/register.json"
 import { AiOutlineGoogle } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import swal from 'sweetalert';
+import { AuthContext } from "../../Hooks/AuthProvider/AuthProvider";
 const From = () => {
+  const [see,setSee]=useState(false);
+  const{userEmail,googleSignIn}=useContext(AuthContext);
+  const handleRegister =e =>{
+    e.preventDefault();
+    const from = e.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const password = from.password.value;
+    const user ={name, email, password}
+    //password condition
+    if(password.length<6){
+      swal("password must be at least 6 characters");
+      return
+    }else if(!/[A-Z]/.test(password)){
+      swal("password must be at least  uppercase letter");
+      return
+
+     }else if(!/(?=.*?[#?!@$%^&*-])/.test(password)){
+      swal("password must be at least a special character");
+      return
+     }else{
+      swal("Good job!", "You are successfully registered", "success");
+     }
+     
+    //email & password  register
+    userEmail(email, password)
+    .then(result =>{console.log(result)})
+    .catch(err =>{console.log(err)});
+    
+   }
+
+   //register  with google
+   const handleGoogle=()=>{
+    googleSignIn()
+    .then(result=>{
+       console.log(result.user)
+       
+    })
+    .then(error=>console.error(error))
+  }
     return (
         <div className="w-[97%] mx-auto m-2 ">
            <div className=" bg-blue-300   dark:bg-violet-300  dark:rounded-lg  lg:w-[90%] lg:mx-auto" >
@@ -12,34 +55,34 @@ const From = () => {
       <Lottie animationData={registerAnimation} className="m-3 h-[250px] md:h-[340px] lg:h-[550px] lg::ml-[160px]"></Lottie>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleRegister}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="text" placeholder="Name" className="input input-bordered" required />
+          <input type="text" placeholder="Name" name="name"  className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" name="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type={see? "text" : "password"} placeholder="password" name="password" className="input input-bordered" required />
           <div className="flex gap-2 justify-between">
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
-          <p className="text-xm  mt-1 ml-[80px]">Show password</p>
+          <span className="text-xm  mt-1 ml-[80px]" onClick={()=>setSee(!see)}>Show password</span>
           </div>
         </div>
         <div className="form-control mt-6">
         <button className="btn btn-outline btn-success text-xl font-bold">Register</button>
-        <button className="btn btn-outline btn-accent text-4xl font-bold mt-4"><AiOutlineGoogle></AiOutlineGoogle></button>
+        <button className="btn btn-outline btn-accent text-4xl font-bold mt-4" onClick={handleGoogle}><AiOutlineGoogle></AiOutlineGoogle></button>
         <div>
             <p className="text-xl font-bold text-black mt-2 text-center">Already have an account<Link to='/login' className="text-red-700">Login</Link></p>
         </div>
