cfd87a0522c51e3df7b18e8db753bc008ff79731
Sizan458 <www.shijan23@gmail.com>
2023-11-06 11:22:29 +0600

    install firebase

---
+++ src/Component/FireBase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSSYRsU3SiHJ9rx_GBnh5ayqrgqlqXgsk",
+  authDomain: "study-ally-11673.firebaseapp.com",
+  projectId: "study-ally-11673",
+  storageBucket: "study-ally-11673.appspot.com",
+  messagingSenderId: "72805662452",
+  appId: "1:72805662452:web:bc05459d66768c46e528a1"
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
+export default App;
